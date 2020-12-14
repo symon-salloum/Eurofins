00fc75912171eaba32d41ebddf9328b35af6b00a
Symon Salloum <symon.salloum@hotmail.com>
2020-12-14 11:40:24 +0100

    AutoMapping Profile

---
+++ EurofinsWebApplication/BusinessLayer/Mapping/ToDoTaskProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BusinessLayer.Domain.ToDoTask;
+using BusinessLayer.DTO;
+
+namespace BusinessLayer.Mapping
+{
+    public class ToDoTaskProfile : Profile
+    {
+        public ToDoTaskProfile()
+        {
+            CreateMap<ToDoTaskDTO, ToDoTask>();
+        }
+    }
+}
