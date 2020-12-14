c3004e43f97ef72a154042651ac71c212b0b5546
Symon Salloum <symon.salloum@hotmail.com>
2020-12-14 11:38:45 +0100

    Adding domain classes and data transfer object (DTO)

---
+++ EurofinsWebApplication/BusinessLayer/DTO/ToDoTaskDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DTO
+{
+    public class ToDoTaskDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Missing Title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Missing Description")]
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ EurofinsWebApplication/BusinessLayer/Domain/ToDoTask/ToDoTask.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.DTO;
+
+namespace BusinessLayer.Domain.ToDoTask
+{
+    public class ToDoTask
+    {
+        public ToDoTask(ToDoTaskDTO toDoTaskDTO)
+        {
+            InitializeFields(toDoTaskDTO);
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+
+        public void InitializeFields(ToDoTaskDTO toDoTaskDTO)
+        {
+            Id = toDoTaskDTO.Id;
+
+            Title = toDoTaskDTO.Title;
+
+            Description = toDoTaskDTO.Description;
+
+            IsCompleted = toDoTaskDTO.IsCompleted;
+        }
+    }
+}
