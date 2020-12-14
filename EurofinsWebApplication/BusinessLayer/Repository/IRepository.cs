d7bd544118d37cd817399de1f1cbeed0898ee111
Symon Salloum <symon.salloum@hotmail.com>
2020-12-14 13:36:17 +0100

    repository and unit of work

--- EurofinsWebApplication/BusinessLayer/Repository/IRepository.cs
+++ EurofinsWebApplication/BusinessLayer/Repository/IRepository.cs
@@ -6,12 +6,18 @@ namespace BusinessLayer.Repository
 {
     public interface IRepository<TEntity> where TEntity : class
     {
-        TEntity GetById(params object[] id);
-        TEntity FirstOrDefault(Expression<Func<TEntity, bool>> where);
+        TEntity GetById(int id);
+
         IQueryable<TEntity> GetAll();
+
+        TEntity FirstOrDefault(Expression<Func<TEntity, bool>> where);
+
         IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> where);
+
         TEntity Add(TEntity entity);
+
         TEntity Update(TEntity entity);
+
         void Delete(TEntity entity);
     }
 }
--- EurofinsWebApplication/BusinessLayer/UnitOfWork/IUnitOfWork.cs
+++ EurofinsWebApplication/BusinessLayer/UnitOfWork/IUnitOfWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessLayer.UnitOfWork
+﻿namespace BusinessLayer.UnitOfWork
 {
     public interface IUnitOfWork
     {
