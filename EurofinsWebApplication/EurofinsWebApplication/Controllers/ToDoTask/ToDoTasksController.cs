930fffd11c8bfbc8476dc48f8c87ad864c8498a9
Symon Salloum <symon.salloum@hotmail.com>
2020-12-15 04:21:39 +0100

    added database file to App_Data and changed connection string

---
+++ EurofinsWebApplication/EurofinsWebApplication/Controllers/ToDoTask/ToDoTasksController.cs
@@ -0,0 +1,98 @@
+﻿using BusinessLayer.DTO;
+using BusinessLayer.Exceptions;
+using BusinessLayer.Filters;
+using BusinessLayer.Service.ToDoTask;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace EurofinsWebApplication.Controllers.ToDoTask
+{
+    public class ToDoTasksController : ApiController
+    {
+        private readonly IToDoTaskService _toDoTaskService;
+
+        public ToDoTasksController(IToDoTaskService toDoTaskService)
+        {
+            _toDoTaskService = toDoTaskService;
+        }
+
+        // GET: api/ToDoTasks/GetAllToDoTasks
+        public IHttpActionResult GetAllToDoTasks()
+        {
+            IEnumerable<ToDoTaskDTO> toDoTaskDTOs = _toDoTaskService.GetAll();
+
+            return Ok(toDoTaskDTOs);
+        }
+
+        // GET: api/ToDoTasks/GetToDoTasks?Title=""&Description=""&IsCompleted={true,false}&IsStricktFilter={true,false}
+        public IHttpActionResult GetToDoTask([FromUri] FilterModel filterModel)
+        {
+            if (!filterModel.IsValidFilter()) return BadRequest(ModelState);
+
+            IEnumerable<ToDoTaskDTO> toDoTaskDTOs = _toDoTaskService.GetMultiple(filterModel);
+
+            return Ok(toDoTaskDTOs);
+        }
+
+        // GET: api/ToDoTasks/GetToDoTask/{id}
+        public IHttpActionResult GetToDoTask(int id)
+        {
+            try
+            {
+                return Ok(_toDoTaskService.GetById(id));
+            }
+            catch (GetEntityException)
+            {
+                return NotFound();
+            }
+        }
+
+        // POST: api/ToDoTasks/PostToDoTask
+        public IHttpActionResult PostToDoTask(ToDoTaskDTO toDoTaskDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            toDoTaskDTO = _toDoTaskService.Create(toDoTaskDTO);
+
+            return CreatedAtRoute("DefaultApi", new { id = toDoTaskDTO.Id }, toDoTaskDTO);
+        }
+
+        // PUT: api/ToDoTasks/PutToDoTask/{id}
+        public IHttpActionResult PutToDoTask(int id, ToDoTaskDTO toDoTaskDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != toDoTaskDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            _toDoTaskService.Update(toDoTaskDTO);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/ToDoTasks/DeleteToDoTask/{id}
+        public IHttpActionResult DeleteToDoTask(int id)
+        {
+            try
+            {
+                var toDoTaskDTO = _toDoTaskService.Delete(id);
+
+                return Ok(toDoTaskDTO);
+            }
+            catch (DeleteEntityException)
+            {
+                return NotFound();
+            }
+        }
+
+    }
+}
--- EurofinsWebApplication/EurofinsWebApplication/Global.asax.cs
+++ EurofinsWebApplication/EurofinsWebApplication/Global.asax.cs
@@ -1,9 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
+using AutoMapper;
+using BusinessLayer.Mapping;
+using EurofinsWebApplication.Injection;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using SimpleInjector.Lifestyles;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace EurofinsWebApplication
 {
@@ -11,6 +13,18 @@ namespace EurofinsWebApplication
     {
         protected void Application_Start()
         {
+            var container = new Container();
+
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            SimpleInjectorInitializer.InitializeContainer(container);//packages from Injection folder 
+
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+
+            container.Verify();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
