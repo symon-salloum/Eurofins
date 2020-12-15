4760c00687f1be40fffe5518f8cc76d4b9912ccd
Symon Salloum <symon.salloum@hotmail.com>
2020-12-15 04:19:18 +0100

    removed MapperProvider

---
+++ EurofinsWebApplication/EurofinsWebApplication/Injection/SimpleInjectorInitializer.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using BusinessLayer.Domain.ToDoTask;
+using BusinessLayer.Mapping;
+using BusinessLayer.Repository;
+using BusinessLayer.Repository.ToDoTask;
+using BusinessLayer.Service.ToDoTask;
+using BusinessLayer.UnitOfWork;
+using DataAccessLayer.DatabaseFactory;
+using DataAccessLayer.Repository;
+using DataAccessLayer.UnitOfWork;
+using SimpleInjector;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace EurofinsWebApplication.Injection
+{
+    public static class SimpleInjectorInitializer
+    {
+        public static void InitializeContainer(Container container)
+        {
+            container.Register<IUnitOfWork, UnitOfWork>();
+            container.Register<IToDoTaskService, ToDoTaskService>();
+            container.Register<IDatabaseFactory, DatabaseFactory>(Lifestyle.Scoped);
+            container.Register<IToDoTaskRepository, ToDoTaskRepository>();
+            container.Register<IRepository<ToDoTask>, Repository<ToDoTask>>();
+
+            //register AutoMapper
+            var config = new MapperConfiguration(cfg => cfg.AddProfile(new ToDoTaskProfile()));
+            container.RegisterInstance(config);
+            container.Register(() => config.CreateMapper(container.GetInstance));
+        }
+    }
+}
